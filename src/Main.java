52eb27c76db9654f7f851d2471d5a1d3f5c196aa
Esteban V.M. <estebanvm90@outlook.com>
2025-04-13 20:27:55 -0300

    controlando el acceso a la escritura de datos

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
+import com.aluracursos.screenmatch.models.Movie;
+
 public class Main {
     public static void main(String[] args) {
-        Movie movie1 = new Movie();
-        movie1.title = "Matrix";
-        movie1.dateOfRelease = 1998;
-        movie1.duration = 120;
-        movie1.showDetails();
+        Movie movie = new Movie();
+        movie.setTitle("Encanto");
+        movie.setDateOfRelease(2021);
+        movie.setDuration(180);
+        movie.setIncludedInPlan(true);
+        movie.showDetails();
+        movie.rate(10);
+        movie.rate(10);
+        System.out.println(movie.calculateAverage());
+        System.out.println(movie.getScoreCounter());
 
-        Movie movie2 = new Movie();
-        movie2.title = "Encanto";
-        movie2.dateOfRelease = 2021;
-        movie2.duration = 180;
-        movie2.showDetails();
-        movie2.rate(10);
-        movie2.rate(10);
-        System.out.println(movie2.totalScore);
-        System.out.println(movie2.calculateAverage());
+//        com.aluracursos.screenmatch.models.Movie movie1 = new com.aluracursos.screenmatch.models.Movie();
+//        movie1.title = "Matrix";
+//        movie1.dateOfRelease = 1998;
+//        movie1.duration = 120;
+//        movie1.showDetails();
+
     }
 }
